9b74862898e20fb5b57de35f313292170ad62236
Nganie <ngan.tp.quan@gmail.com>
2018-05-06 12:24:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(katz_deli)
-  if line.count == 0
+  if katz_deli.count == 0
     puts "The line is currently empty."
   elsif array.length == 2
     return array.join(" and ")

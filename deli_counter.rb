5a6b0402b621dd69f5b4e2423617515d2bcd890a
Nganie <ngan.tp.quan@gmail.com>
2018-05-06 12:52:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,13 @@
 # Write your code here.
-
-katz_deli = []
 def line(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.count == 0
     puts "The line is currently empty."
-  elsif katz_deli.length >= 0
-    puts "The line is currently:"
+    return
+  else
+    count = "The line is currently:"
+    katz_deli.each_with_index do |name,index|
+      line_number = index + 1
+      count << "#{line_number}. #{name}"
   end
-end
-
-def take_a_number(katz_deli)
-end
-
-def now_serving(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  end
+puts count
 end

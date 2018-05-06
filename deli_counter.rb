6c9eabdba038f9b9a0e6f99efb71c46543d0e506
Nganie <ngan.tp.quan@gmail.com>
2018-05-06 13:14:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(katz_deli)
-  if katz_deli = []
+  if katz_deli.count == 0
     puts "The line is currently empty."
   else
     greeting = "The line is currently:"

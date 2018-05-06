65272f70ff9cb94c7c527fc91584be82c035ea23
Nganie <ngan.tp.quan@gmail.com>
2018-05-06 12:59:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,10 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.count == 0
+    puts "There is nobody waiting to be served!"
+    katz_deli.shift
+  end
+end

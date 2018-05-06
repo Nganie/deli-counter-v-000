b5608f6f9bea35fdabce3110080199bc1eed7e0e
Nganie <ngan.tp.quan@gmail.com>
2018-05-06 13:05:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,13 @@
 # Write your code here.
 def line(katz_deli)
-  if katz_deli.count == 0
-    puts "The line is currently empty."
-    return
-  else
-    count = "The line is currently:"
+  if katz_deli.count != 0
+    msg = "The line is currently:"
     katz_deli.each_with_index do |name,index|
-      line_number = index + 1
-      count << "#{line_number}. #{name}"
-  end
-  puts count
+      msg << "#{index.to_i+1}. #{name}"
+    end
+    puts msg
+  else
+  puts "The line is currently empty."
   end
 end
 

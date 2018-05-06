cfab06a55cb151e4c85449b2b384bbaa456ca651
Nganie <ngan.tp.quan@gmail.com>
2018-05-06 12:30:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,12 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif katz_deli.length >= 0
     puts "The line is currently:"
+  end
 end
+
+def take_a_number(katz_deli)
 end
+
+def now_serving(katz_deli)
+end
+
